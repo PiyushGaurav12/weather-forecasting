0e702a2ddb3eff1a596adfcd8e6dbf47269a8610
Piyush Mehta <darksungaming9@gmail.com>
2025-04-19 08:58:24 +0530

    updated pm 2.6 section

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import useAutoUpdate from '../hooks/useAutoUpdate'; // adjust path
 import "../styles/styles.css";
 import co2 from "../images/co2.png";
 import co from "../images/co.png";
-import pm25 from "../images/pm25.png";
+// import pm25 from "../images/pm25.png";
 import temperature from "../images/temperature.png";
 import humidity from "../images/humidity.png";
 
@@ -20,10 +20,10 @@ const Header = () => {
         <img src={co} alt="CO" className="sensor-icon" />
         <span>CO average <br /> <strong>{data?.field3 || 'Loading...'} ppm</strong></span>
       </div>
-      <div className="header-item">
+      {/* <div className="header-item">
         <img src={pm25} alt="PM2.5" className="sensor-icon" />
         <span>PM2.5 Good <br /> <strong>{data?.field5 || 'Loading...'} µm/m³</strong></span>
-      </div>
+      </div> */}
       <div className="header-item">
         <img src={temperature} alt="Temperature" className="sensor-icon" />
         <span>Temperature <br /> <strong>{data?.field1 || 'Loading...'}°C</strong></span>
